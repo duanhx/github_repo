63a07950980a24d5069a2bd295c25e5dfac3d0ed
duanhx <duanhongxuan@sohu.com>
2015-10-23 19:56:41 +0800

    AppFuse SSH demo project.

---
+++ struts/src/main/java/cn/edu/nepu/appfuse/service/impl/PasswordTokenManager.java
@@ -0,0 +1,19 @@
+package cn.edu.nepu.appfuse.service.impl;
+
+import cn.edu.nepu.appfuse.model.User;
+
+
+public interface PasswordTokenManager {
+
+    /**
+     * {@inheritDoc}
+     */
+    String generateRecoveryToken(User user);
+
+    /**
+     * {@inheritDoc}
+     */
+    boolean isRecoveryTokenValid(User user, String token);
+
+    void invalidateRecoveryToken(User user, String token);
+}
